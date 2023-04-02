205590f64ef7f33251438277560b8447dc7ad777
Jason Barr <jason@jasonsbarr.com>
2023-04-02 15:42:33 -0500

    Add class to represent source location

---
+++ src/reader/SrcLoc.js
@@ -0,0 +1,33 @@
+/**
+ * @typedef SrcLoc
+ * @property {number} line
+ * @property {number} col
+ * @property {number} pos
+ * @property {string} file
+ */
+class SrcLoc {
+  /**
+   * Constructs a SrcLoc instance
+   * @param {number} line
+   * @param {number} col
+   * @param {number} pos
+   * @param {string} file
+   */
+  constructor(line, col, pos, file) {
+    this.line = line;
+    this.col = col;
+    this.pos = pos;
+    this.file = file;
+  }
+
+  /**
+   * Static SrcLoc constructor
+   * @param {number} line
+   * @param {number} col
+   * @param {number} pos
+   * @param {string} file
+   */
+  static new(line, col, pos, file) {
+    return new SrcLoc(line, col, pos, file);
+  }
+}
