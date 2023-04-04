9e9632a09566b0752df9d46a9333a507eac9c282
Jason Barr <jason@jasonsbarr.com>
2023-04-04 12:51:30 -0500

    Stub out macro read functions

--- packages/reader/src/MacroReader.js
+++ packages/reader/src/MacroReader.js
@@ -1,4 +1,4 @@
-import { cons, Cons } from '@arith-lang/core';
+import { cons, Cons, SyntaxException } from '@arith-lang/core';
 import { Token, TokenTypes } from '@arith-lang/lexer';
 import { TokenReader } from './TokenReader';
 /**
@@ -37,5 +37,47 @@ export class MacroReader {
    * Dispatcher for reading the various macros
    * @param {Token} token
    */
-  readMacro(token) {}
+  read(token) {
+    switch (token.type) {
+      case TokenTypes.Quote:
+        return this.readQuote();
+      case TokenTypes.QQuote:
+        return this.readQuasiquote();
+      case TokenTypes.SUQuote:
+        return this.readSpliceUnquote();
+      case TokenTypes.UQuote:
+        return this.readUnquote();
+      case TokenTypes.Hash:
+        return this.readHashMacro(token);
+      default:
+        throw new SyntaxException(`Unknown reader macro ${token.value}`, token.srcloc);
+    }
+  }
+
+  /**
+   * Reads a # macro
+   * @param {Token} token
+   */
+  readHashMacro(token) {}
+
+  /**
+   * Reads a ` macro as a quasiquote expression
+   */
+  readQuasiquote() {}
+
+  /**
+   * Reads a ' macro as a quote expression
+   * @returns {Cons}
+   */
+  readQuote() {}
+
+  /**
+   * Reads a ~@ macro as a splice-unquote expression
+   */
+  readSpliceUnquote () {}
+
+  /**
+   * Reads a ~ macro as an unquote expression
+   */
+  readUnquote() {}
 }
