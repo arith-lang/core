be70bcc668d603b677340764a7e4a4aa3220bc87
Jason Barr <jason@jasonsbarr.com>
2023-04-04 12:12:00 -0500

    Export TokenTypes for reader

--- packages/lexer/index.js
+++ packages/lexer/index.js
@@ -2,5 +2,6 @@ import { tokenize } from "./src/tokenize.js";
 import { Token } from "./Token.js";
 import { TokenBag } from "./TokenBag.js";
 import { SrcLoc } from "./src/SrcLoc.js";
+import { TokenTypes } from "./src/TokenTypes.js";
 
-export { tokenize, SrcLoc, Token, TokenBag };
+export { tokenize, SrcLoc, Token, TokenBag, TokenTypes };
