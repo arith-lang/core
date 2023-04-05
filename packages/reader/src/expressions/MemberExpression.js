14c89abe886416d5167fe3bc3796883b401da518
Jason Barr <jason@jasonsbarr.com>
2023-04-04 21:51:23 -0500

    Define classes for MemberExpression and OptionalMemberExpression

--- packages/reader/src/expressions/MemberExpression.js
+++ packages/reader/src/expressions/MemberExpression.js
@@ -1,1 +1,43 @@
-export class MemberExpression {}
+import { SrcLoc, Token } from "@arith-lang/lexer";
+
+/**
+ * @typedef {import("../CST").CST} CST
+ */
+/**
+ * @class MemberExpression
+ * @desc For object property access
+ * @property {string} type
+ * @property {CST} object
+ * @property {Token} property
+ * @property {SrcLoc} srcloc
+ * @property {string} code
+ */
+
+export class MemberExpression {
+  /**
+   * Constructs a MemberExpression
+   * @param {CST} object
+   * @param {Token} property
+   * @param {SrcLoc} srcloc
+   * @param {string} code
+   */
+  constructor(object, property, srcloc, code) {
+    this.type = "MemberExpression";
+    this.object = object;
+    this.property = property;
+    this.srcloc = srcloc;
+    this.code = code;
+  }
+
+  /**
+   * Static constructor for MemberExpression
+   * @param {CST} object
+   * @param {Token} property
+   * @param {SrcLoc} srcloc
+   * @param {string} code
+   * @returns {MemberExpression}
+   */
+  static new(object, property, srcloc, code) {
+    return new MemberExpression(object, property, srcloc, code);
+  }
+}
--- packages/reader/src/expressions/OptionalMemberExpression.js
+++ packages/reader/src/expressions/OptionalMemberExpression.js
@@ -1,1 +1,43 @@
-export class OptionalMemberExpression {}
+import { SrcLoc, Token } from "@arith-lang/lexer";
+
+/**
+ * @typedef {import("../CST").CST} CST
+ */
+/**
+ * @class OptionalMemberExpression
+ * @desc For object property access
+ * @property {string} type
+ * @property {CST} object
+ * @property {Token} property
+ * @property {SrcLoc} srcloc
+ * @property {string} code
+ */
+
+export class OptionalMemberExpression {
+  /**
+   * Constructs a OptionalMemberExpression
+   * @param {CST} object
+   * @param {Token} property
+   * @param {SrcLoc} srcloc
+   * @param {string} code
+   */
+  constructor(object, property, srcloc, code) {
+    this.type = "OptionalMemberExpression";
+    this.object = object;
+    this.property = property;
+    this.srcloc = srcloc;
+    this.code = code;
+  }
+
+  /**
+   * Static constructor for OptionalMemberExpression
+   * @param {CST} object
+   * @param {Token} property
+   * @param {SrcLoc} srcloc
+   * @param {string} code
+   * @returns {OptionalMemberExpression}
+   */
+  static new(object, property, srcloc, code) {
+    return new OptionalMemberExpression(object, property, srcloc, code);
+  }
+}
