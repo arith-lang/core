a1ef48f67bd8ed7f93e631bf9b6db924c96d90f1
Jason Barr <jason.barr@metronet.com>
2023-05-05 12:12:18 -0500

    Add fail util function

--- src/core/utils.js
+++ src/core/utils.js
@@ -1,7 +1,13 @@
+import { Exception } from "./exceptions.js";
+
 export function isNullish(obj) {
   return obj == null;
 }
 
 export function hasMethod(obj, methodName) {
   return !isNullish(obj?.[methodName]) && typeof obj[methodName] === "function";
+}
+
+export function fail(msg, exn = Exception) {
+  throw new exn(msg);
 }
