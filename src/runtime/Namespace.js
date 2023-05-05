659632b6e3ce0c792eea5cf9d9cb7b4b5f5542eb
Jason Barr <jason.barr@metronet.com>
2023-05-05 16:16:53 -0500

    Create Namespace class

---
+++ src/runtime/Namespace.js
@@ -0,0 +1,57 @@
+import { ReferenceException } from "../core/exceptions";
+
+export class Namespace {
+  constructor({ parent = null, initial = null } = {}) {
+    this.parent = parent;
+    this.vars = new Map();
+
+    if (initial) {
+      this.addMany(initial);
+    }
+  }
+
+  addMany(vars) {
+    for (let [k, v] of vars instanceof Map ? vars : Object.entries(vars)) {
+      this.vars.set(k, v);
+    }
+
+    return this;
+  }
+
+  // traverses the chain of namespaces
+  exists(name) {
+    return this.lookup(name) !== null;
+  }
+
+  get(name) {
+    const scope = this.lookup(name);
+
+    if (scope) {
+      return scope.vars.get(name);
+    }
+
+    throw new ReferenceException(name);
+  }
+
+  // checks only in the current namespace
+  has(name) {
+    return this.vars.has(name);
+  }
+
+  lookup(name) {
+    let current = this;
+
+    while (current !== null) {
+      if (current.vars.has(name)) {
+        return current;
+      }
+      current = this.parent;
+    }
+
+    return null;
+  }
+
+  set(name, value) {
+    this.vars.set(name, value);
+  }
+}
