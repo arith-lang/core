5f91739d220931dc3be2050ce0396df8a0939b7b
Jason Barr <jason.barr@metronet.com>
2023-05-10 20:19:47 -0500

    Stub out expander

---
+++ src/expander/expand.js
@@ -0,0 +1,11 @@
+import { Cons } from "../core/cons.js";
+
+// just returns the s-expression tree produced by the reader for now
+/**
+ *
+ * @param {Cons} prog
+ * @returns
+ */
+export function expand(prog) {
+  return prog;
+}
