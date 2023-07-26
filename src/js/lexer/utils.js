d3e005db57b8e3775506222b8ce9df40edc439d8
Jason Barr <jason.barr@metronet.com>
2023-07-26 15:04:59 -0500

    Move number matchers to own file

--- src/js/lexer/utils.js
+++ src/js/lexer/utils.js
@@ -30,19 +30,3 @@ export const isAlphaNumeric = (ch) => /[0-9a-fA-F]/.test(ch);
 // String matchers
 export const isBoolean = (str) => /true|false/.test(str);
 export const isNil = (str) => /nil/.test(str);
-export const isRational = (str) => /^[\+\-]?\d+\/\d+$/.test(str);
-export const isComplex = (str) =>
-  /^[\+\-]?(\d+(\.\d+)?\+)?\d+(\.\d+)?i$/.test(str);
-
-const DECIMAL = String.raw`^[+-]?\d+\.\d+(e[+-]\d+)?$`;
-const FLOAT = new RegExp(DECIMAL + "f");
-const REAL = new RegExp(DECIMAL);
-const HEX = String.raw`0x[0-9a-fA-F]+`;
-const BIN = String.raw`0b[0-1]+`;
-const OCT = String.raw`0o[0-7]+`;
-const INT = String.raw`\d+`;
-const INTEGER = new RegExp(`^[+-]?(${HEX}|${OCT}|${BIN}|${INT})$`);
-
-export const isFloat = (str) => FLOAT.test(str);
-export const isReal = (str) => REAL.test(str);
-export const isInteger = (str) => INTEGER.test(str);
---
+++ src/js/shared/number-matchers.js
@@ -0,0 +1,23 @@
+const DECIMAL = String.raw`^[+-]?\d+\.\d+(e[+-]\d+)?$`;
+const FLOAT = new RegExp(DECIMAL + "f");
+const REAL = new RegExp(DECIMAL);
+const HEX = String.raw`0x[0-9a-fA-F]+`;
+const BIN = String.raw`0b[0-1]+`;
+const OCT = String.raw`0o[0-7]+`;
+const INT = String.raw`\d+`;
+const INTEGER = new RegExp(`^[+-]?(${HEX}|${OCT}|${BIN}|${INT})$`);
+
+export const isRational = (str) => /^[\+\-]?\d+\/\d+$/.test(str);
+export const isComplex = (str) =>
+  /^[\+\-]?(\d+(\.\d+)?\+)?\d+(\.\d+)?i$/.test(str);
+export const isFloat = (str) => FLOAT.test(str);
+export const isReal = (str) => REAL.test(str);
+export const isInteger = (str) => INTEGER.test(str);
+
+export default {
+  isRational,
+  isComplex,
+  isFloat,
+  isReal,
+  isInteger,
+};
