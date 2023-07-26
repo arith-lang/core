d2980c604b023bbfb00a3797a3e1dbd49319966b
Jason Barr <jason.barr@metronet.com>
2023-07-26 07:57:23 -0500

    Add lexer utils

--- src/js/lexer/utils.js
+++ src/js/lexer/utils.js
@@ -1,1 +1,25 @@
-export const isNewline = (ch) => ch === "\n";
+// Character matchers
+export const isDot = (ch) => /\./.test(ch);
+export const isDigit = (ch) => /\d/.test(ch);
+export const isWhitespace = (ch) => /[\s,]/.test(ch);
+export const isSemicolon = (ch) => /;/.test(ch);
+export const isNewline = (ch) => /\n/.test(ch);
+export const isDash = (ch) => /\-/.test(ch);
+export const isPlus = (ch) => /\+/.test(ch);
+export const isDoubleQuote = (ch) => /"/.test(ch);
+export const isColon = (ch) => /:/.test(ch);
+export const isSymbolStart = (ch) => /[=<>%|?\\/*\p{L}_$!+-]/u.test(ch);
+export const isSymbolChar = (ch) =>
+  /[=@~<>%&|?\\/^*&#'\p{L}\p{N}_$!+-]/u.test(ch);
+export const isLParen = (ch) => /\(/.test(ch);
+export const isRParen = (ch) => /\)/.test(ch);
+export const isLBrack = (ch) => /\[/.test(ch);
+export const isRBrack = (ch) => /\]/.test(ch);
+export const isLBrace = (ch) => /\{/.test(ch);
+export const isRBrace = (ch) => /\}/.test(ch);
+export const isAmp = (ch) => /&/.test(ch);
+
+// String matchers
+export const isNumber = (str) => /^[+-]?\d+(\.\d+)?$/.test(str);
+export const isBoolean = (str) => /true|false/.test(str);
+export const isNil = (str) => /nil/.test(str);
