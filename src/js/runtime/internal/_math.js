aba5d17c3f56efd7cae740b30a5ef3b881d7a331
Jason Barr <jason.barr@metronet.com>
2023-07-26 15:26:27 -0500

    Start configuring math.js instance to work with BigInt

---
+++ src/js/runtime/internal/_math.js
@@ -0,0 +1,35 @@
+import { create, all, factory } from "mathjs";
+
+const config = {
+  number: "BigNumber",
+};
+
+export const math = create(all, config);
+
+math.import([
+  factory(
+    "BigInt",
+    ["typed"],
+    function createBigInt({ typed }) {
+      typed.addType({
+        name: "BigInt",
+        test: (x) => typeof x === "bigint",
+      });
+
+      return BigInt;
+    },
+    { lazy: false },
+  ),
+
+  factory(
+    "bigint",
+    ["typed", "BigInt"],
+    function createBigint({ typed, BigInt }) {
+      return typed("bigint", {
+        "number | string": (x) => BigInt(x),
+      });
+    },
+  ),
+]);
+
+export default math;
