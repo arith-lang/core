0270d7502fbe084a5fb949064e4664b34c58e451
Jason Barr <jason.barr@metronet.com>
2023-08-07 10:41:19 -0500

    Read whitespace and comments as trivia

--- src/js/lexer/lexer.js
+++ src/js/lexer/lexer.js
@@ -1,3 +1,12 @@
+import {
+  isDash,
+  isDigit,
+  isNewline,
+  isPlus,
+  isSemicolon,
+  isWhitespace,
+} from "./utils.js";
+
 export class Lexer {
   /**
    * Constructor
@@ -14,6 +23,37 @@ export class Lexer {
    */
   static new(input) {
     return new Lexer(input);
+  }
+
+  /**
+   * Reads a number from the input
+   * @param {string} trivia
+   * @returns {import("./token").Token}
+   */
+  readNumber(trivia) {}
+
+  /**
+   * Tokenizes an input stream
+   * @returns {import("./token").Token[]}
+   */
+  tokenize() {
+    /** @type {import("./token").Token[]} */
+    let tokens = [];
+    let trivia = "";
+
+    while (!this.input.eof()) {
+      let ch = this.input.peek();
+      if (isWhitespace(ch)) {
+        trivia += this.input.readWhile(isWhitespace);
+      } else if (isSemicolon(ch)) {
+        trivia += this.input.readWhile((ch) => !isNewline(ch));
+      } else if (isDash(ch) || isPlus(ch)) {
+        if (isDigit(this.input.lookahead(1))) {
+          tokens.push(this.readNumber(trivia));
+          trivia = "";
+        }
+      }
+    }
   }
 }
 
