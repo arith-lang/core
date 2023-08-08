2a63fb94a1809fd84abdc9edad98292f1ea27051
Jason Barr <jason.barr@metronet.com>
2023-08-08 12:36:08 -0500

    Create diagnostics object for read-syntax

--- src/js/reader/read-syntax.js
+++ src/js/reader/read-syntax.js
@@ -1,5 +1,10 @@
+import DiagnosticBag from "../shared/diagnostics/diagnostic-bag.js";
+
 /**
  * Reads the token stream into a data structure for the compiler
  * @param {import("../lexer/lexer").LexerOutput} lexResult
  */
-export const readSyntax = (lexResult) => {};
+export const readSyntax = (lexResult) => {
+  const { tokens, diagnostics: lexerDiagnostics, input } = lexResult;
+  let diagnostics = DiagnosticBag.from(lexerDiagnostics);
+};
