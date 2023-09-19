e86d8a6d82e52f684dc6fbc2f53f9c1e922e7c17
Jason Barr <jason.barr@metronet.com>
2023-09-19 12:30:58 -0500

    Add begin token to reader output by default

--- src/js/reader/read-syntax.js
+++ src/js/reader/read-syntax.js
@@ -1,5 +1,8 @@
 import DiagnosticBag from "../shared/diagnostics/diagnostic-bag.js";
 import Reader from "./reader.js";
+import { cons } from "../shared/cons.js";
+import { makeToken } from "../shared/make-token.js";
+import TokenTypes from "../lexer/token-types.js";
 
 /**
  * Reads the token stream into a data structure for the compiler
@@ -9,4 +12,13 @@ export const readSyntax = (lexResult) => {
   const { tokens, diagnostics: lexerDiagnostics, input } = lexResult;
   const diagnostics = DiagnosticBag.from(lexerDiagnostics);
   const reader = Reader.new(tokens, diagnostics, input);
+  const first = tokens[0];
+
+  if (!first) {
+    // Use default empty srcloc
+    return cons(makeToken(TokenTypes.Symbol, "begin"), null); // change this to reader output with diagnostics
+  }
+
+  const srcloc = first.srcloc;
+  const output = cons(makeToken(TokenTypes.Symbol, "begin", srcloc), null);
 };
