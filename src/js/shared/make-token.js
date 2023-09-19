2a024321da422ba70c3aac944b131cfa65e8e9a4
Jason Barr <jason.barr@metronet.com>
2023-09-19 12:26:18 -0500

    Make srcloc a param, not file name

--- src/js/shared/make-token.js
+++ src/js/shared/make-token.js
@@ -1,10 +1,15 @@
 import SrcLoc from "../lexer/src-loc.js";
 import Token from "../lexer/token.js";
+import SrcLoc from "../lexer/src-loc.js";
 
 /**
  * Creates a new arbitrary token to add to the reader output or in expansion
  * @param {import("../lexer/token-types.js").TokenTypes} type
  * @param {string} value
+ * @param {SrcLoc} srcloc
  */
-export const makeToken = (type, value) =>
-  Token.new(type, value, SrcLoc.new(0, 0, 0, "created"), "");
+export const makeToken = (
+  type,
+  value,
+  srcloc = SrcLoc.new(0, 0, 0, "created"),
+) => Token.new(type, value, srcloc, "");
