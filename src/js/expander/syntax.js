ec41cb60a7efcdf208cfeede9e8d05b8de58439e
Jason Barr <jason.barr@metronet.com>
2023-09-19 17:48:03 -0500

    Create Syntax class for use in expander

---
+++ src/js/expander/syntax.js
@@ -0,0 +1,30 @@
+/**
+ * Represents a syntactic form in the macro expander
+ */
+export class Syntax {
+  /**
+   * Constructor
+   * @param {string} name
+   * @param {Function} evaluator
+   */
+  constructor(name, evaluator) {
+    this.name = name;
+    this.evaluator = evaluator;
+  }
+
+  /**
+   * Static constructor
+   * @param {string} name
+   * @param {Function} evaluator
+   * @returns {Syntax}
+   */
+  static new(name, evaluator) {
+    return new Syntax(name, evaluator);
+  }
+
+  evaluate(form) {
+    return this.evaluator(form);
+  }
+}
+
+export default Syntax;
