0e791bc767818b3e9a608d60181def3e2276006d
Jason Barr <jason.barr@metronet.com>
2023-09-20 13:07:37 -0500

    Add object functions to runtime object

--- src/js/runtime/create-runtime.js
+++ src/js/runtime/create-runtime.js
@@ -1,12 +1,14 @@
 import * as assertions from "./assertions.js";
 import * as keyword from "./keyword.js";
 import * as number from "./number.js";
+import * as object from "./object.js";
 
 export const createRuntime = () => {
   return {
     ...assertions,
     ...keyword,
     ...number,
+    ...object,
   };
 };
 
