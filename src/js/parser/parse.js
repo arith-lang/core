caf32a234b3a5560e91e1e02c8d7216b1cf9755d
Jason Barr <jason.barr@metronet.com>
2023-09-26 09:28:30 -0500

    Rename file

--- src/js/parser/parse.js
+++ src/js/parser/parse.js
@@ -5,13 +5,13 @@ import AST from "./ast.js";
 
 /**
  * @typedef ParserOutput
- * @prop {import("./ast").AST} ast
+ * @prop {import("./ast.js").AST} ast
  * @prop {DiagnosticBag} diagnostics
  */
 
 /**
  * Parses expanded reader output into a full AST
- * @param {import("../reader/read-syntax").ReaderOutput} readResult
+ * @param {import("../reader/read-syntax.js").ReaderOutput} readResult
  */
 export const parse = (readResult) => {
   const { ast: forms, diagnostics: readerDiagnostics, input } = readResult;
